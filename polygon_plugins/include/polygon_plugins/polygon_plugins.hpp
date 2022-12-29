3d61e80dde2350d832ae1c2740f1a30adc2dbf3f
panxxhub <panezion@outlook.com>
2022-12-29 17:09:24 +0800

    init project

---
+++ polygon_plugins/include/polygon_plugins/polygon_plugins.hpp
@@ -0,0 +1,40 @@
+#ifndef POLYGON_PLUGINS__POLYGON_PLUGINS_HPP_
+#define POLYGON_PLUGINS__POLYGON_PLUGINS_HPP_
+
+#include <cmath>
+#include <polygon_base/regular_polygon.hpp>
+#include "polygon_plugins/visibility_control.h"
+
+namespace polygon_plugins
+{
+
+class Square : public polygon_base::RegularPolygon
+{
+public:
+  void initialize(double side_length) override { side_length_ = side_length; }
+
+  auto area() -> double override { return side_length_ * side_length_; }
+
+protected:
+  double side_length_;
+};
+
+class Triangle : public polygon_base::RegularPolygon
+{
+public:
+  void initialize(double side_length) override { side_length_ = side_length; }
+
+  auto area() -> double override { return 0.5 * side_length_ * get_height(); }
+
+  auto get_height() const -> double
+  {
+    return std::sqrt((side_length_ * side_length_) - ((side_length_ / 2) * (side_length_ / 2)));
+  }
+
+protected:
+  double side_length_;
+};
+
+}  // namespace polygon_plugins
+
+#endif  // POLYGON_PLUGINS__POLYGON_PLUGINS_HPP_
---
+++ polygon_plugins/src/polygon_plugins.cpp
@@ -0,0 +1,11 @@
+#include "polygon_plugins/polygon_plugins.hpp"
+
+namespace polygon_plugins
+{
+
+}  // namespace polygon_plugins
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(polygon_plugins::Square, polygon_base::RegularPolygon)
+PLUGINLIB_EXPORT_CLASS(polygon_plugins::Triangle, polygon_base::RegularPolygon)
